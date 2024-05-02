0056b2486171a25d83b72d74fad30b94255a01df
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-05-02 12:18:54 +0200

    remove comment

--- models/example/job_execution_tracking.sql
+++ models/example/job_execution_tracking.sql
@@ -7,5 +7,5 @@
 }}
 
 SELECT
-    '{{ run_started_at.astimezone(modules.pytz.timezone("Europe/Paris")).strftime("%Y-%m-%d %H:%M:%S") }}'  AS date_time_load -- date
+    '{{ run_started_at.astimezone(modules.pytz.timezone("Europe/Paris")).strftime("%Y-%m-%d %H:%M:%S") }}'  AS date_time_load
     , '{{ invocation_id }}'                                                                                 AS run_id
