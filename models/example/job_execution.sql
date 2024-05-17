075527b9cc176d10bc7fdb5876909b35b6db93f8
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-05-17 15:09:23 +0200

    update

--- models/example/job_execution.sql
+++ models/example/job_execution.sql
@@ -8,4 +8,4 @@
 
 SELECT
     '{{ run_started_at.astimezone(modules.pytz.timezone("Europe/Paris")).strftime("%Y-%m-%d %H:%M:%S") }}'  AS date_time_load
-    , '{{ invocation_id }}'                                                                                 AS run_id -- comment
+    , '{{ invocation_id }}'                                                                                 AS run_id -- modified element
