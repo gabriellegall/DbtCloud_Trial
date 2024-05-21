2135d5fd734bd40767ddb844676a9b8acc1ab3be
Gabriel LE GALL <gabrielgall@hotmail.fr>
2024-05-21 21:48:23 +0200

    stg layer

---
+++ models/staging/stg_lydia__payments.sql
@@ -0,0 +1,6 @@
+SELECT 
+    CAST(member_id AS text)                         AS payment_user_id
+    , CAST(operation_id AS text)                    AS payment_id
+    , TO_DATE(date, 'DD/MM/YYYY')                   AS payment_user_card_activation_date
+    , CAST(amount AS decimal)                       AS payment_amount
+FROM {{ ref("payments") }}
